5feb6a265f911e330e44211827cbcea0eaf7238f
Jean-Marie Mirebeau <jm.mirebeau@gmail.com>
2025-03-24 09:32:09 +0100

    Cleanup

--- agdt/Selling.py
+++ agdt/Selling.py
@@ -15,7 +15,7 @@ class Selling:
         sel = Selling(m.n, m.dtype)
 
         Taichi kernel scope : 
-        for ... in ... : 
+        for ... in ... : # Within some outer loop
             λ,e = sel.Selling(m)
     """
 
@@ -109,135 +109,95 @@ class Selling:
             """Selling decomposition of m"""
             return Decomp(m, Superbase(m))
 
-        self.Superbase,self.Decomp,self.Selling = Superbase,Decomp,Selling
+        @ti.func
+        def Reconstruct(λ:ti.template(),e:ti.template()):
+            """
+            Computes Sum_i λi ei ei^T
+            Inputs : 
+             - λ:weights
+             - e:offsets
+            """
+            m = mat(0)
+            for i in range(λ.n):
+                m += λ[i] * e[i,:].outer_product(e[i,:])
+            return m
 
 
+        self.Superbase,self.Decomp,self.Selling,self.Reconstruct = Superbase,Decomp,Selling,Reconstruct
 
+def DecompWithFixedOffsets(λ,e,base=256):
+    """
+    Input : 
+    - λ : array of reals (n1,...,nk, n)
+    - e : array of integer vectors (n1,...,nk, n,d) (Opposite vectors are regarded as identical)
 
+    Output : 
+    - λ : array of reas (n1,...,nk, N)
+    - e : array of integer vectors (N,d)
+    """
 
-def mk_Selling(ndim, float_t=ti.f32,offset_t=ti.i8,nitermax=100):
-    """Bluids a class that computes the Selling decomposition"""
+    assert λ.shape == e.shape[:-1]
+    shape = λ.shape[:-1]
+    n = λ.shape[-1]
+    ndim = e.shape[-1]
 
+    λ = λ.reshape(-1,n)
+    e = e.reshape(-1,n,ndim)
 
-    symdim = (ndim*(ndim+1))//2
-    vec = ti.lang.matrix.VectorType(ndim,float_t)
-    mat = ti.lang.matrix.MatrixType(ndim,ndim,2,float_t)
-    superbase = ti.lang.matrix.MatrixType(ndim+1,ndim,2,offset_t)
-    offsets = ti.lang.matrix.MatrixType(symdim,ndim,2,offset_t)
-    weights = ti.lang.matrix.VectorType(symdim,float_t)
-    
-    if ndim==2:
-        cycle = ti.lang.matrix.MatrixType(symdim,ndim+1,2,ti.i32) ( (0,1,2),(1,2,0),(2,0,1) )
-    elif ndim==3:
-        cycle = ti.lang.matrix.MatrixType(symdim,ndim+1,2,ti.i32) ( 
-        (0,1,2,3),(0,2,1,3),(0,3,1,2),(1,2,0,3),(1,3,0,2),(2,3,0,1) )
+    float_t = λ.dtype
+    offset_t = e.dtype
+    int_t = ti.i64
 
-    # ----------- One dimensional ---------
+    vec = ti.lang.matrix.VectorType(ndim,offset_t)
     @ti.func
-    def Superbase1(m : mat):
-        """Compute an m-obtuse superbase, where m is symmetric positive definite"""
-        return superbase((1,))
+    def index(v:vec):
+        res:int_t = 0
+        sign:int_t = 0
+        b = 1
+        for i in v:
+            if sign==0: sign = (v[i]>0) - (v[i]<0) # ti.sign ? 
+            res += sign*v[i]*b
+            b *= base
+        return res
 
-    @ti.func
-    def Decomp1(m : mat, b : superbase):
-        """Decompose m using a given superbase"""
-        return weights(m[0,0]), b
+    @ti.kernel
+    def compute_indices(e: ti.field(dtype=vec), ie : ti.field(dtype=int_t) ):
+        for i in e: ie[i] = index(e[i])
+    ie = ti.field(int_t, shape=λ.shape)
+    compute_indices(e,ie)
 
-    @ti.func
-    def Selling1(m : mat):
-        """Selling decomposition of m"""
-        return Decomp1(m,Superbase1(m))
+    return
 
-    # ----------- Two dimensional ---------
-    @ti.func
-    def Superbase2(m : mat): 
-        """Compute an m-obtuse superbase, where m is symmetric positive definite"""
-        b = superbase((1,0),(0,1),(-1,-1)) # Canonical superbase
-        npass:ti.i32=0
-        for niter in range(nitermax):
-            i,j,k = cycle[niter%cycle.n,:]
-            if b[i,:]@m@b[j,:]>0: # Check if the angle is acute
-                npass=0
-                b[k,:] =   b[j,:] - b[i,:]
-                b[j,:] = - b[j,:]
-            else:
-                npass+=1
-                if npass==cycle.n: break
-        return b
+    # Get the unique index values
+    ie_unique,ie_index,ie_inverse = np.unique(ie,return_index=True,return_inverse=True)
 
-    @ti.func
-    def Decomp2(m : mat, e : superbase):
-        """Decompose m using a given superbase"""
-        λ = - weights(e[1,:]@m@e[2,:], e[0,:]@m@e[2,:], e[0,:]@m@e[1,:])
-        for i in range(e.n):
-            e[i,0],e[i,1] = -e[i,1],e[i,0] # Compute perpendicular vectors
-        return λ,e #weights,offsets
+    # The new offsets
+    N = len(ie_unique) # Number of different offsets
+    E = e[ie_index,:] # Collection of all different offsets
 
-    @ti.func
-    def Selling2(m : mat):
-        """Selling decomposition of m"""
-        return Decomp2(m, Superbase2(m))
+    # The new weights
+    @ti.kernel
+    def set_coefficients(
+        λ: ti.field(dtype=float_t), 
+        ie_inverse: ti.field(dtype=int_t),
+        Λ: ti.field(dtype=float_t)
+        ):
+        for i,j in λ:
+            J = ie_inverse[i,j]
+            Λ[i,J] = λ[i,j]
+    Λ = ti.field(float_t,shape = (*shape,N))
+    set_coefficients(λ,ie_inverse,Λ)
 
-    # ---------- Three dimensional --------
-    @ti.func
-    def Superbase3(m:mat):
-        """Compute an m-obtuse superbase, where m is symmetric positive definite"""
-        b = superbase((1,0,0),(0,1,0),(0,0,1),(-1,-1,-1)) # Canonical superbase
-        npass:ti.i32=0
-        for niter in range(nitermax):
-            i,j,k,l = cycle[niter%cycle.n,:]
-            if b[i,:]@m@b[j,:]>0: # Check if the angle is acute
-                npass=0
-                b[k,:] += b[j,:] 
-                b[l,:] += b[j,:]
-                b[j,:] = - b[j,:]
-            else:
-                npass+=1
-                if npass==cycle.n: break
-        return b
-
-    @ti.func
-    def Decomp3(m:mat, b:superbase):
-        """Decompose m using a given superbase"""
-        λ = weights(0)
-        e = offsets(0)
-        for n in range(cycle.n):
-            i,j,k,l = cycle[n,:]
-            λ[n] = - b[i,:]@m@b[j,:]
-            e[n,:] = b[k,:].cross(b[l,:])
-        return λ,e
-
-    @ti.func 
-    def Selling3(m:mat):
-        """Selling decomposition of m"""
-        return Decomp3(m,Superbase3(m))
-
-    Superbase,Decomp,Selling = [None,(Superbase1,Decomp1,Selling1),
-        (Superbase2,Decomp2,Selling2),(Superbase3,Decomp3,Selling3)][ndim]
-
-#    @ti.dataclass
-    
-        # Constants
-#        nonlocal symdim
- #       Selling = 
-
-        #symdim = symdim
-
-        # Types
-        #vec = vec
-
-        # Static functions
-#        Superbase = Superbase
- #       Decomp = Decomp
-  #      Selling = Selling
-
- #   Selling.Superbase = Superbase
- #   Selling.Decomp = Decomp
- #   Selling.vec = vec
+    return Λ,E
 
 
 
-    return Selling
 
-#def mk_Selling3(float_t=ti.f32,offset_t=ti.i8,nitermax=100):
+
+
+
+
+
+
+
 
